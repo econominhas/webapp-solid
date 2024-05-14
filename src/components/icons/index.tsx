644600124a784cb2820820f44a85d974b0590e6a
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-05-14 10:37:27 -0300

    feat: make auth form to login with google and facebook

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,9 @@
+import { Command } from "lucide-solid";
+import { Facebook } from "./facebook";
+import { Google } from "./google";
+
+export const Icons = {
+  logo: Command,
+  google: Google,
+  facebook: Facebook,
+};
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,3 +1,30 @@
+import { Icons } from "@/components/icons";
+import { Button } from "@/components/ui/button";
+
 export function SignIn() {
-  return <main>Sign in</main>;
+  return (
+    <main>
+      <div class="container flex h-screen w-screen flex-col items-center justify-center">
+        <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div class="flex flex-col text-center">
+            <Icons.logo class="mx-auto h-6 w-6 mb-2" />
+            <h1 class="text-2xl font-semibold tracking-tight">
+              Welcome to Econominhas
+            </h1>
+            <p class="text-sm text-muted-foreground">
+              Enter your email to sign in to your account
+            </p>
+          </div>
+          <div class="flex items-center gap-2">
+            <Button variant="outline" class="flex-1">
+              <Icons.google class="size-4" />
+            </Button>
+            <Button variant="outline" class="flex-1">
+              <Icons.facebook class="size-4" />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
